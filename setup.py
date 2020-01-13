e587ee28874658688880a48591c4cb77e9a30828
Guido Schmitz <guido.schmitz@devolo.de>
2020-01-13 17:39:49 +0100

    Preperations for getting a pypi module

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="devolo_home_control_api", # Replace with your own username
+    version="0.0.1",
+    author="Markus Bong, Guido Schmitz",
+    author_email="guido.schmitz@fedaix.de",
+    description="devolo Home Control API in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/2Fake/devolo_home_control_api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
