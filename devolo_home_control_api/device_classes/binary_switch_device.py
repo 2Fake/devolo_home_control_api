20b92422d4e730a55cf1c5b360c8f71ff0437383
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-17 19:13:02 +0100

    Introduce logger
    Beside that, imports where fixed according the new init files.

--- devolo_home_control_api/device_classes/binary_switch_device.py
+++ devolo_home_control_api/device_classes/binary_switch_device.py
@@ -1,6 +1,6 @@
-from device_classes.device import Device
-from property_classes.binary_switch_property import BinarySwitchProperty
-from property_classes.consumption_property import ConsumptionProperty
+from ..property_classes.binary_switch_property import BinarySwitchProperty
+from ..property_classes.consumption_property import ConsumptionProperty
+from .device import Device
 
 
 class BinarySwitchDevice(Device):
@@ -10,15 +10,16 @@ class BinarySwitchDevice(Device):
         self.binary_switch_property = {}
 
         for element_uid in element_uids:
+            self._logger.debug(f"Handling element UID: {element_uid}")
             if element_uid.startswith('devolo.Meter'):
                 self.consumption_property[element_uid] = ConsumptionProperty(element_uid=element_uid)
             elif element_uid.startswith('devolo.BinarySwitch'):
                 self.binary_switch_property[element_uid] = BinarySwitchProperty(element_uid=element_uid)
             else:
-                pass
-                # TODO: Log this!
+                self._logger.info(f"Found an unexpected element UID: {element_uid}")
+                self._logger.info("This element UID will be ignored for now.")
 
         if len(self.consumption_property) == 0:
             del self.consumption_property
         if len(self.binary_switch_property) == 0:
-            raise TypeError('Created a binary switch device without binary switch.')
+            raise TypeError("Created a binary switch device without binary switch.")
