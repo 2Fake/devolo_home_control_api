2da4a26cf472fee6c5fb23368c6a09bee8d0e6a9
Markus Bong <Markus.Bong@devolo.de>
2020-01-17 08:34:46 +0100

    made values public

--- devolo_home_control_api/device_classes/binary_switch_device.py
+++ devolo_home_control_api/device_classes/binary_switch_device.py
@@ -6,20 +6,14 @@ from property_classes.consumption_property import ConsumptionProperty
 class BinarySwitchDevice(Device):
     def __init__(self, name, fim_uid, element_uids):
         super().__init__(name=name, fim_uid=fim_uid)
-        self._consumption_property = []
-        self._binary_switch_property = []
+        self.consumption_property = []
+        self.binary_switch_property = []
 
         for element_uid in element_uids:
             if element_uid.startswith('devolo.Meter'):
-                self._consumption_property.append(ConsumptionProperty(element_uid=element_uid))
+                self.consumption_property.append(ConsumptionProperty(element_uid=element_uid))
             elif element_uid.startswith('devolo.BinarySwitch'):
-                self._binary_switch_property.append(BinarySwitchProperty(element_uid=element_uid))
+                self.binary_switch_property.append(BinarySwitchProperty(element_uid=element_uid))
             else:
                 pass
                 # TODO: Log this!
-
-    def get_binary_switch_property(self):
-        return self._binary_switch_property
-
-    def get_consumption_property(self):
-        return self._consumption_property
--- devolo_home_control_api/property_classes/binary_switch_property.py
+++ devolo_home_control_api/property_classes/binary_switch_property.py
@@ -4,10 +4,4 @@ from property_classes.property import Property
 class BinarySwitchProperty(Property):
     def __init__(self, element_uid):
         super().__init__(element_uid=element_uid)
-        self._state = None
-
-    def get_state(self):
-        return self._state
-
-    def set_state(self, state: bool):
-        self._state = state
+        self.state = None
