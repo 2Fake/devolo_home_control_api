20101e5b1517bc869c06ece48a442230253a2406
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-23 22:39:28 +0100

    Rename MprmWebSocket to MprmWebsocket

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import logging
 
-from devolo_home_control_api.mprm_websocket import MprmWebSocket
+from devolo_home_control_api.mprm_websocket import MprmWebsocket
 from devolo_home_control_api.mydevolo import Mydevolo
 
 logging.basicConfig(level=logging.DEBUG)
@@ -22,9 +22,8 @@ mydevolo.user = user
 mydevolo.password = password
 
 gateway_id = mydevolo.gateway_ids[0]
-gateway = mydevolo.get_gateway(gateway_id)
-mprm_websocket = MprmWebSocket(user=user, password=password, gateway_id=gateway_id)
+mprm_websocket = MprmWebsocket(gateway_id=gateway_id)
 
 for device in mprm_websocket.devices:
-        mprm_websocket.devices[device].subscriber = Subscriber(device)
-        mprm_websocket.publisher.register(device, mprm_websocket.devices[device].subscriber)
+    mprm_websocket.devices[device].subscriber = Subscriber(device)
+    mprm_websocket.publisher.register(device, mprm_websocket.devices[device].subscriber)
