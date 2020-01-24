ac01b03cbcc75ff23de4bc1718a95588ec97b38d
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-24 21:56:24 +0100

    Add class description

--- devolo_home_control_api/devices/zwave.py
+++ devolo_home_control_api/devices/zwave.py
@@ -2,6 +2,12 @@ import logging
 
 
 class Zwave:
+    """
+    Representing object for Z-Wave devices connected to the devolo Home Control Central Unit
+
+    :param device_uid: Device UID, something like hdm:ZWave:CBC56091/24
+    """
+
     def __init__(self, name, device_uid):
         self._logger = logging.getLogger(self.__class__.__name__)
         self.name = name
