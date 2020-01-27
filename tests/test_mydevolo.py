a80655ec775341f3465bc9bd33cbca037c5aaac6
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-27 16:46:06 +0100

    Add two test cases

--- tests/test_mydevolo.py
+++ tests/test_mydevolo.py
@@ -1,4 +1,5 @@
 import pytest
+
 from devolo_home_control_api.mydevolo import Mydevolo
 
 
@@ -6,8 +7,31 @@ class TestMydevolo():
 
     def test_uuid(self, mocker):
         def _call_mock(url):
-            return {"uuid": "123456789"}
+            return {"uuid": "535512AB-165D-11E7-A4E2-000C29D76CCA"}
 
         mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
-        md = Mydevolo.get_instance()
-        assert md.uuid == "123456789"
+        mydevolo = Mydevolo.get_instance()
+        assert mydevolo.uuid == "535512AB-165D-11E7-A4E2-000C29D76CCA"
+
+    def test_gateway_ids(self, mocker):
+        def _call_mock(url):
+            return {"items": [{"gatewayId": "1409301750000598"}]}
+
+        mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
+        mydevolo = Mydevolo.get_instance()
+        mydevolo._uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+        mydevolo._gateway_ids = []
+
+        assert mydevolo.gateway_ids == ["1409301750000598"]
+
+    def test_gateway_ids_empty(self, mocker):
+        def _call_mock(url):
+            return {"items": []}
+
+        mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
+        mydevolo = Mydevolo.get_instance()
+        mydevolo._uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+        mydevolo._gateway_ids = []
+
+        with pytest.raises(IndexError):
+            mydevolo.gateway_ids
