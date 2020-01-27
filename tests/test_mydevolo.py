0f8e494759be9909b590c899fa3d6298c0c9db8f
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-27 21:25:20 +0100

    Experiment with test data

--- tests/test_mydevolo.py
+++ tests/test_mydevolo.py
@@ -5,25 +5,28 @@ from devolo_home_control_api.mydevolo import Mydevolo
 
 class TestMydevolo():
 
+    _uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+    _gateway = "1409301750000598"
+
     def test_uuid(self, mocker):
         def _call_mock(url):
-            return {"uuid": "535512AB-165D-11E7-A4E2-000C29D76CCA"}
+            return {"uuid": TestMydevolo._uuid}
 
         mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
         Mydevolo.del_instance()
         mydevolo = Mydevolo.get_instance()
-        assert mydevolo.uuid == "535512AB-165D-11E7-A4E2-000C29D76CCA"
+        assert mydevolo.uuid == TestMydevolo._uuid
 
     def test_gateway_ids(self, mocker):
         def _call_mock(url):
-            return {"items": [{"gatewayId": "1409301750000598"}]}
+            return {"items": [{"gatewayId": TestMydevolo._gateway}]}
 
         mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
         Mydevolo.del_instance()
         mydevolo = Mydevolo.get_instance()
-        mydevolo._uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+        mydevolo._uuid = TestMydevolo._uuid
 
-        assert mydevolo.gateway_ids == ["1409301750000598"]
+        assert mydevolo.gateway_ids == [TestMydevolo._gateway]
 
     def test_gateway_ids_empty(self, mocker):
         def _call_mock(url):
@@ -32,7 +35,33 @@ class TestMydevolo():
         mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
         Mydevolo.del_instance()
         mydevolo = Mydevolo.get_instance()
-        mydevolo._uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+        mydevolo._uuid = TestMydevolo._uuid
 
         with pytest.raises(IndexError):
             mydevolo.gateway_ids
+
+    def test_get_gateway(self, mocker):
+        def _call_mock(url):
+            return {"gatewayId": TestMydevolo._gateway}
+
+        mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
+        Mydevolo.del_instance()
+        mydevolo = Mydevolo.get_instance()
+        mydevolo._uuid = TestMydevolo._uuid
+
+        details = mydevolo.get_gateway(TestMydevolo._gateway)
+
+        assert details.get("gatewayId") == TestMydevolo._gateway
+
+    def test_get_full_url(self, mocker):
+        def _call_mock(url):
+            return {"url": "https://homecontrol.mydevolo.com/dhp/portal/fullLogin/?token=1410000000002_1:ec73a059f398fa8b&X-MPRM-LB=1410000000002_1"}
+
+        mocker.patch("devolo_home_control_api.mydevolo.Mydevolo._call", side_effect=_call_mock)
+        Mydevolo.del_instance()
+        mydevolo = Mydevolo.get_instance()
+        mydevolo._uuid = "535512AB-165D-11E7-A4E2-000C29D76CCA"
+
+        full_url = mydevolo.get_full_url(TestMydevolo._gateway)
+
+        assert full_url == "https://homecontrol.mydevolo.com/dhp/portal/fullLogin/?token=1410000000002_1:ec73a059f398fa8b&X-MPRM-LB=1410000000002_1"
