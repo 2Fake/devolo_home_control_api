ce635b0b0819c2dcc58fa1cfce04f8242dc5a912
Markus Bong <2Fake1987@gmail.com>
2020-01-28 14:44:09 +0100

    mock init of gateway

--- tests/test_mprm_rest.py
+++ tests/test_mprm_rest.py
@@ -1,13 +1,11 @@
 import pytest
 
 from devolo_home_control_api.mprm_rest import MprmRest
-
 from .mock_gateway import Gateway
 
 
 class TestMprmRest:
-
-    def test_binary_switch_devices(self, mocker, mock_gateway):
-        mock_gateway.return_value = Gateway("1409301750000598")
+    def test_binary_switch_devices(self, mocker):
+        mocker.patch('devolo_home_control_api.devices.gateway.Gateway.__init__', Gateway.__init__)
         mprm = MprmRest("1409301750000598")
         print(mprm._gateway)
