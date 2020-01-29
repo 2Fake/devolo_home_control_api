2e7bd6ccda9379986ee3b94f8084a200d44eee6b
Guido Schmitz <guido.schmitz@fedaix.de>
2020-01-29 21:49:20 +0100

    Add one test case

--- tests/test_mprm_rest.py
+++ tests/test_mprm_rest.py
@@ -1,19 +1,21 @@
 import pytest
 
 from devolo_home_control_api.mprm_rest import MprmRest
+from tests.mock_metering_plug import metering_plug
 
 
 class TestMprmRest:
-    #def test_binary_switch_devices(self, mock_gateway, mock_inspect_devices, mock_mprmrest__detect_gateway_in_lan):
-    #    MprmRest("1409301750000598")
+    def test_binary_switch_devices(self, mock_gateway, mock_inspect_devices_metering_plug, mock_mprmrest__detect_gateway_in_lan):
+        mprm = MprmRest(self.gateway_id)
+        assert hasattr(mprm.binary_switch_devices[0], "binary_switch_property")
 
-    def test_get_binary_switch_state_invalid(self, mock_gateway, mock_inspect_devices, mock_mprmrest__detect_gateway_in_lan):
+    def test_get_binary_switch_state_invalid(self, mock_gateway, mock_inspect_devices_metering_plug, mock_mprmrest__detect_gateway_in_lan):
         mprm = MprmRest(self.gateway_id)
 
         with pytest.raises(ValueError):
             mprm.get_binary_switch_state("invalid")
 
-    def test_get_consumption_invalid(self, mock_gateway, mock_inspect_devices, mock_mprmrest__detect_gateway_in_lan):
+    def test_get_consumption_invalid(self, mock_gateway, mock_inspect_devices_metering_plug, mock_mprmrest__detect_gateway_in_lan):
         mprm = MprmRest(self.gateway_id)
 
         with pytest.raises(ValueError):
@@ -21,7 +23,7 @@ class TestMprmRest:
         with pytest.raises(ValueError):
             mprm.get_consumption("devolo.Meter:", "invalid")
 
-    def test_get_voltage_invalid(self, mock_gateway, mock_inspect_devices, mock_mprmrest__detect_gateway_in_lan):
+    def test_get_voltage_invalid(self, mock_gateway, mock_inspect_devices_metering_plug, mock_mprmrest__detect_gateway_in_lan):
         mprm = MprmRest(self.gateway_id)
 
         with pytest.raises(ValueError):
