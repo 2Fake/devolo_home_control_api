87c34c6b64140e91aebb15a78189bf02cd6812b3
Markus Bong <2Fake1987@gmail.com>
2020-01-30 11:04:30 +0100

    setting property fully tested now

---
+++ tests/test_setting_property.py
@@ -0,0 +1,22 @@
+import pytest
+from devolo_home_control_api.properties.settings_property import SettingsProperty
+from devolo_home_control_api.properties.property import WrongElementError
+
+
+class TestSettingsProperty:
+    def test_settings_property_valid(self):
+        setting_property = SettingsProperty(f"lis.{self.device_uid}",
+                                            led_setting=True,
+                                            events_enabled=False,
+                                            param_changed=True,
+                                            local_switching=False,
+                                            remote_switching=True)
+        assert setting_property.led_setting
+        assert not setting_property.events_enabled
+        assert setting_property.param_changed
+        assert not setting_property.local_switching
+        assert setting_property.remote_switching
+
+    def test_settings_property_invalid(self):
+        with pytest.raises(WrongElementError):
+            SettingsProperty(self.device_uid)
