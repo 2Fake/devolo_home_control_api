179ac4aef08cb7163786f45d3a1673662a5bbd4a
Markus Bong <2Fake1987@gmail.com>
2020-01-31 12:15:28 +0100

    correct order in logger config

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import logging
 from devolo_home_control_api.mprm_websocket import MprmWebsocket
 from devolo_home_control_api.mydevolo import Mydevolo
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
 user = "username"
 password = "password"
