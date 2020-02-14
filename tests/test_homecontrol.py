7626d1444649ce9619dd9aad86b49c7af30e239c
Markus Bong <2Fake1987@gmail.com>
2020-02-14 10:28:56 +0100

    add two testcases

--- tests/test_homecontrol.py
+++ tests/test_homecontrol.py
@@ -7,3 +7,10 @@ import pytest
 class TestHomeControl:
     def test_binary_switch_devices(self):
         assert hasattr(self.homecontrol.binary_switch_devices[0], "binary_switch_property")
+
+    def test_get_publisher(self):
+        assert len(self.homecontrol.publisher._events) == 3
+
+    def test_is_online(self):
+        assert self.homecontrol.is_online(self.devices.get("mains").get("uid"))
+        assert not self.homecontrol.is_online(self.devices.get("ambiguous_2").get("uid"))
