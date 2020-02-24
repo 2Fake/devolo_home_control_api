ca7695acce8d95fb29de86d1cf8a2bf3d3f6d914
Markus Bong <2Fake1987@gmail.com>
2020-02-24 09:45:14 +0100

    sort import

--- devolo_home_control_api/publisher/updater.py
+++ devolo_home_control_api/publisher/updater.py
@@ -1,9 +1,9 @@
 import json
 import logging
 
-from ..devices.zwave import get_device_type_from_element_uid, get_device_uid_from_element_uid
 from .publisher import Publisher
 from ..devices.gateway import Gateway
+from ..devices.zwave import get_device_type_from_element_uid, get_device_uid_from_element_uid
 
 
 class Updater:
