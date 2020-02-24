8a54e2731a03a8a0d6ea4b65c055fe5d2b700e91
Guido Schmitz <guido.schmitz@fedaix.de>
2020-02-24 22:32:26 +0100

    Add doctypes

--- tests/fixtures/homecontrol.py
+++ tests/fixtures/homecontrol.py
@@ -9,6 +9,7 @@ from ..mocks.mock_homecontrol import mock__inspect_devices
 @pytest.fixture()
 def home_control_instance(request, mydevolo, mock_gateway, mock_mprmwebsocket_websocket_connection,
                           mock_inspect_devices_metering_plug, mock_mprmrest__detect_gateway_in_lan):
+    """ Create a mocked Home Control instance with static test data. """
     request.cls.homecontrol = HomeControl(request.cls.gateway.get("id"))
     request.cls.homecontrol.devices['hdm:ZWave:F6BF9812/4'] \
         .binary_switch_property['devolo.BinarySwitch:hdm:ZWave:F6BF9812/4'].is_online = request.cls.homecontrol.is_online
@@ -18,9 +19,11 @@ def home_control_instance(request, mydevolo, mock_gateway, mock_mprmwebsocket_we
 
 @pytest.fixture()
 def mock_inspect_devices_metering_plug(mocker, mock_mydevolo__call):
+    """ Create a mocked Home Control setup with a Metering Plug. """
     mocker.patch("devolo_home_control_api.homecontrol.HomeControl._inspect_devices", mock__inspect_devices)
 
 
 @pytest.fixture()
 def mock_homecontrol_is_online(mocker):
+    """ Let the gateway be offline all the time. """
     mocker.patch("devolo_home_control_api.homecontrol.HomeControl.is_online", return_value=False)
