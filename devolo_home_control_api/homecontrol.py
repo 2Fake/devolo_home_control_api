e758898a65501b60356916afad37a62546f939ee
Markus Bong <2Fake1987@gmail.com>
2020-02-24 10:06:03 +0100

    remove complexity

--- devolo_home_control_api/homecontrol.py
+++ devolo_home_control_api/homecontrol.py
@@ -73,6 +73,22 @@ class HomeControl:
         """ Initialize steps needed to update properties on a new message. """
         self.updater.update(message)
 
+    def _binary_switch(self, device: str, element_uid: str):
+        if not hasattr(self.devices[device], "binary_switch_property"):
+            self.devices[device].binary_switch_property = {}
+        self._logger.debug(f"Adding binary switch property to {device}.")
+        self.devices[device].binary_switch_property[element_uid] = BinarySwitchProperty(element_uid)
+        self.devices[device].binary_switch_property[element_uid].is_online = self.is_online
+        self.devices[device].binary_switch_property[element_uid].fetch_binary_switch_state()
+
+    def _general_device(self, device:str, setting_uid: str):
+        self._logger.debug(f"Adding general device settings to {device}.")
+        self.devices[device].settings_property["general_device_settings"] = SettingsProperty(element_uid=setting_uid,
+                                                                                             events_enabled=None,
+                                                                                             name=None,
+                                                                                             zone_id=None,
+                                                                                             icon=None)
+        self.devices[device].settings_property["general_device_settings"].fetch_general_device_settings()
 
     def _inspect_devices(self):
         """ Create the initial internal device dict. """
@@ -82,85 +98,67 @@ class HomeControl:
             self._process_element_uids(device=device, element_uids=properties.get("elementUIDs"))
             self._process_settings_uids(device=device, setting_uids=properties.get("settingUIDs"))
 
+    def _led(self, device:str, setting_uid: str):
+        self._logger.debug(f"Adding led settings to {device}.")
+        self.devices[device].settings_property["led"] = SettingsProperty(element_uid=setting_uid, led_setting=None)
+        self.devices[device].settings_property["led"].fetch_led_setting()
+
+    def _meter(self, device: str, element_uid: str):
+        if not hasattr(self.devices[device], "consumption_property"):
+            self.devices[device].consumption_property = {}
+        self._logger.debug(f"Adding consumption property to {device}.")
+        self.devices[device].consumption_property[element_uid] = ConsumptionProperty(element_uid)
+        self.devices[device].consumption_property[element_uid].fetch_consumption('current')
+        self.devices[device].consumption_property[element_uid].fetch_consumption('total')
+
+    def _parameter(self, device: str, setting_uid: str):
+        self._logger.debug(f"Adding parameter settings to {device}.")
+        self.devices[device].settings_property["param_changed"] = SettingsProperty(element_uid=setting_uid,
+                                                                                   param_changed=None)
+        self.devices[device].settings_property["param_changed"].fetch_param_changed_setting()
+
     def _process_element_uids(self, device: str, element_uids: list):
         """ Generate properties depending on the element uid. """
-        def binary_switch(element_uid: str):
-            if not hasattr(self.devices[device], "binary_switch_property"):
-                self.devices[device].binary_switch_property = {}
-            self._logger.debug(f"Adding binary switch property to {device}.")
-            self.devices[device].binary_switch_property[element_uid] = BinarySwitchProperty(element_uid)
-            self.devices[device].binary_switch_property[element_uid].is_online = self.is_online
-            self.devices[device].binary_switch_property[element_uid].fetch_binary_switch_state()
-
-        def meter(element_uid: str):
-            if not hasattr(self.devices[device], "consumption_property"):
-                self.devices[device].consumption_property = {}
-            self._logger.debug(f"Adding consumption property to {device}.")
-            self.devices[device].consumption_property[element_uid] = ConsumptionProperty(element_uid)
-            self.devices[device].consumption_property[element_uid].fetch_consumption('current')
-            self.devices[device].consumption_property[element_uid].fetch_consumption('total')
-
-        def voltage_multi_level_sensor(element_uid: str):
-            if not hasattr(self.devices[device], "voltage_property"):
-                self.devices[device].voltage_property = {}
-            self._logger.debug(f"Adding voltage property to {device}.")
-            self.devices[device].voltage_property[element_uid] = VoltageProperty(element_uid)
-            self.devices[device].voltage_property[element_uid].fetch_voltage()
-
-        device_type = {"devolo.BinarySwitch": binary_switch,
-                       "devolo.Meter": meter,
-                       "devolo.VoltageMultiLevelSensor": voltage_multi_level_sensor}
+        device_type = {"devolo.BinarySwitch": self._binary_switch,
+                       "devolo.Meter": self._meter,
+                       "devolo.VoltageMultiLevelSensor": self._voltage_multi_level_sensor}
 
         for element_uid in element_uids:
             try:
-                device_type[get_device_type_from_element_uid(element_uid)](element_uid)
+                device_type[get_device_type_from_element_uid(element_uid)](device, element_uid)
             except KeyError:
                 self._logger.debug(f"Found an unexpected element uid: {element_uid}")
 
+    def _protection(self, device:str, setting_uid: str):
+        self._logger.debug(f"Adding protection settings to {device}.")
+        self.devices[device].settings_property["protection"] = SettingsProperty(element_uid=setting_uid,
+                                                                                local_switching=None,
+                                                                                remote_switching=None)
+        self.devices[device].settings_property["protection"].fetch_protection_setting(protection_setting="local")
+        self.devices[device].settings_property["protection"].fetch_protection_setting(protection_setting="remote")
+
     def _process_settings_uids(self, device: str, setting_uids: list):
         """ Generate properties depending on the setting uid. """
-        def led(setting_uid: str):
-            self._logger.debug(f"Adding led settings to {device}.")
-            self.devices[device].settings_property["led"] = SettingsProperty(element_uid=setting_uid, led_setting=None)
-            self.devices[device].settings_property["led"].fetch_led_setting()
-
-        def general_device(setting_uid: str):
-            self._logger.debug(f"Adding general device settings to {device}.")
-            self.devices[device].settings_property["general_device_settings"] = SettingsProperty(element_uid=setting_uid,
-                                                                                                 events_enabled=None,
-                                                                                                 name=None,
-                                                                                                 zone_id=None,
-                                                                                                 icon=None)
-            self.devices[device].settings_property["general_device_settings"].fetch_general_device_settings()
-
-        def parameter(setting_uid: str):
-            self._logger.debug(f"Adding parameter settings to {device}.")
-            self.devices[device].settings_property["param_changed"] = SettingsProperty(element_uid=setting_uid,
-                                                                                       param_changed=None)
-            self.devices[device].settings_property["param_changed"].fetch_param_changed_setting()
-
-        def protection(setting_uid: str):
-            self._logger.debug(f"Adding protection settings to {device}.")
-            self.devices[device].settings_property["protection"] = SettingsProperty(element_uid=setting_uid,
-                                                                                    local_switching=None,
-                                                                                    remote_switching=None)
-            self.devices[device].settings_property["protection"].fetch_protection_setting(protection_setting="local")
-            self.devices[device].settings_property["protection"].fetch_protection_setting(protection_setting="remote")
-
         if not hasattr(self.devices[device], "settings_property"):
             self.devices[device].settings_property = {}
 
-        setting = {"lis.hdm": led,
-                   "gds.hdm": general_device,
-                   "cps.hdm": parameter,
-                   "ps.hdm": protection}
+        setting = {"lis.hdm": self._led,
+                   "gds.hdm": self._general_device,
+                   "cps.hdm": self._parameter,
+                   "ps.hdm": self._protection}
 
         for setting_uid in setting_uids:
             try:
-                setting[get_device_type_from_element_uid(setting_uid)](setting_uid)
+                setting[get_device_type_from_element_uid(setting_uid)](device, setting_uid)
             except KeyError:
                 self._logger.debug(f"Found an unexpected element uid: {setting_uid}")
 
+    def _voltage_multi_level_sensor(self, device: str, element_uid: str):
+        if not hasattr(self.devices[device], "voltage_property"):
+            self.devices[device].voltage_property = {}
+        self._logger.debug(f"Adding voltage property to {device}.")
+        self.devices[device].voltage_property[element_uid] = VoltageProperty(element_uid)
+        self.devices[device].voltage_property[element_uid].fetch_voltage()
 
 def get_sub_device_uid_from_element_uid(element_uid: str) -> int:
     """
