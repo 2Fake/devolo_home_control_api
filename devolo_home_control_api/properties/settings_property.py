3a00f99d3d49c89006de8785e6a5b1a7c0359b42
Markus Bong <2Fake1987@gmail.com>
2020-05-05 14:54:27 +0200

    add setter for settings

--- devolo_home_control_api/properties/settings_property.py
+++ devolo_home_control_api/properties/settings_property.py
@@ -25,3 +25,93 @@ class SettingsProperty(Property):
         self.setting_uid = element_uid
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+        setter = {"cps": self.set_cps,
+                  "gds": self.set_gds,
+                  "lis": self.set_lis,
+                  "mss": self.set_mss,
+                  "ps": self.set_ps,
+                  "trs": self.set_trs,
+                  "vfs": self.set_vfs
+                  }
+
+        self.setter = setter.get(element_uid.split(".")[0])
+
+    def set_cps(self):
+        pass
+
+    def set_gds(self, **kwargs):
+        """
+        Setter for general device settings.
+        :param kwargs:
+            events_enabled: Boolean of the target value for the diary entry of a device
+            icon: String of the target icon.
+            name: String of the target name.
+            zone_id: String of target zone_id. ATTENTION: This is NOT the name of the location.
+        :return:
+        """
+        allowed = ["events_enabled", "icon", "name", "zone_id"]
+        [setattr(self, item, kwargs.get(item, getattr(self, item))) for item in allowed]
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [{"events_enabled": self.events_enabled,
+                                                       "icon": self.icon,
+                                                       "name": self.name,
+                                                       "zone_id": self.zone_id}]]}
+        self.post(data)
+
+    def set_lis(self, target_value: bool):
+        """
+        Setter for led settings.
+        :param target_value: Boolean of the led indication setting.
+        """
+        self.led_setting = target_value
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [self.led_setting]]}
+        self.post(data)
+
+    def set_mss(self, target_value: int):
+        """
+        Setter for motion sensitivity setting.
+        :param target_value: Integer for the motion sensitivity setting.
+        :raises WrongElementError: If target value is not in range of 0 to 100.
+        """
+        if not 0 <= target_value <= 100:
+            raise WrongElementError("Value should be between 0 and 100")
+        self.motion_sensitivity = target_value
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [self.motion_sensitivity]]}
+        self.post(data)
+
+    def set_ps(self, **kwargs):
+        """
+        Setter for the protection settings.
+        :param: **kwargs:
+            local_switching: Boolean of the target value for local switching
+            remote_switching: Boolean of the target value for remote switching
+        """
+        allowed = ["local_switching", "remote_switching"]
+        [setattr(self, item, kwargs.get(item, getattr(self, item))) for item in allowed]
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [{"localSwitch": self.local_switching,
+                                                       "remoteSwitch": self.remote_switching}]]}
+        self.post(data)
+
+    def set_trs(self, target_value: bool):
+        """
+        Setter for temperature report setting.
+        :param target_value: Boolean of the target value
+        """
+        self.temp_report = target_value
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [self.temp_report]]}
+        self.post(data)
+
+    def set_vfs(self, target_value):
+        """
+        Setter for visual feedback setting.
+        :param target_value: Boolean of the target value
+        """
+        self.led_setting = target_value
+        data = {"method": "FIM/invokeOperation",
+                "params": [self.element_uid, "save", [self.led_setting]]}
+        self.post(data)
