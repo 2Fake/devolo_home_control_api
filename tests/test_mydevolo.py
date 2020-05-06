3567f1408716f07a0c06e9b312dac03969992b40
Guido Schmitz <guido.schmitz@fedaix.de>
2020-05-06 22:29:29 +0200

    Remove duplicate test

--- tests/test_mydevolo.py
+++ tests/test_mydevolo.py
@@ -109,13 +109,6 @@ class TestMydevolo:
             mydevolo._call("test")
         Mydevolo.del_instance()
 
-    @pytest.mark.usefixtures("mock_mydevolo__call_raise_GatewayOfflineError")
-    def test_call_GatewayOfflineError(self):
-        mydevolo = Mydevolo()
-        with pytest.raises(GatewayOfflineError):
-            mydevolo._call("test")
-        Mydevolo.del_instance()
-
     @pytest.mark.usefixtures("mock_response_gateway_offline")
     def test_call_GatewayOfflineError(self, mydevolo):
         with pytest.raises(GatewayOfflineError):
