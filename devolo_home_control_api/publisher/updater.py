512617160241aa74a692da751337b37e55fe8683
Markus Bong <2Fake1987@gmail.com>
2020-06-09 10:25:34 +0200

    fix an issue in device_events --> bool object has no attribute .get()

--- devolo_home_control_api/publisher/updater.py
+++ devolo_home_control_api/publisher/updater.py
@@ -249,11 +249,12 @@ class Updater:
     def _device_events(self, message: dict):
         """ If an operation was not successful, we need to correct our internal state. """
         properties = {"properties": message.get("properties").get("property.value.new")}
-        properties.get("properties")["uid"] = properties.get("properties").get("widgetElementUID")
-        if get_device_type_from_element_uid(properties.get("properties").get("widgetElementUID")) == "devolo.BinarySwitch":
-            properties.get("properties")["property.name"] = "state"
-            properties.get("properties")["property.value.new"] = int(properties.get("properties").get("data"))
-        self.update(properties)
+        if type(properties.get("properties")) is dict:
+            properties.get("properties")["uid"] = properties.get("properties").get("widgetElementUID")
+            if get_device_type_from_element_uid(properties.get("properties").get("widgetElementUID")) == "devolo.BinarySwitch":
+                properties.get("properties")["property.name"] = "state"
+                properties.get("properties")["property.value.new"] = int(properties.get("properties").get("data"))
+            self.update(properties)
 
     def _device_online_state(self, message: dict):
         """ Update the device's online state. """
