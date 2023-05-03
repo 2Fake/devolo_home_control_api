a03bc817d4b516e6bcfabf25c47666d6af745756
Guido Schmitz <guido.schmitz@fedaix.de>
2023-05-03 12:27:51 +0000

    Fix lru_cache

--- tests/__init__.py
+++ tests/__init__.py
@@ -31,7 +31,7 @@ def get_fixtures_path() -> Path:
     return Path(__file__).parent.joinpath("fixtures")
 
 
-@lru_cache
+@lru_cache()
 def load_fixture(name: str) -> Dict[str, Any]:
     """Load a fixture."""
     fixture_path = get_fixtures_path().joinpath(f"{name}.json")
